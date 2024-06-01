1231af1c07f7db5d533a10c0941268fb06a534b3
Igor Piliaiev <igorpilaev27@gmail.com>
2024-06-01 21:03:33 +0300

    add back

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import pkg from 'pg';
+const { Pool } = pkg;
+
+
+
+
+
 
 
 
@@ -13,24 +20,62 @@ dotenv.config()
 
 const app = express();
 
-  
+
 
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'))
 
-
-app.use('/api/users', authRouter)
-app.use("/api/contacts", contactsRouter);
-
-app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'snakegame',
+  password: 'Igoresha1713',
+  port: 5432,
 });
 
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
+
+app.get('/gamer', async (req, res) => {
+  try {
+    console.log('Received request for /gamer');
+    const result = await pool.query('SELECT * FROM gamer');
+    console.log('Database query result:', result.rows);
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error during database query:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+app.post('/gamer', async (req, res) => {
+  const { first_name, score } = req.body;
+  if (!first_name || !score) {
+    return res.status(400).json({ error: 'first_name and score are required' });
+  }
+
+  try {
+    const result = await pool.query(
+      'INSERT INTO gamer (first_name, score) VALUES ($1, $2) RETURNING *',
+      [first_name, score]
+    );
+    console.log('Inserted data:', result.rows[0]);
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error('Error during data insertion:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+
+
+
 });
 
 
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-import app from "./app.js";
-import mongoose from "mongoose";
+// import app from "./app.js";
+// import mongoose from "mongoose";
 
 
 
-const { DB_HOST, PORT = 3000 } = process.env;
+// const { DB_HOST, PORT = 3000 } = process.env;
 
 
 
-mongoose.set('strictQuery', true);
+// mongoose.set('strictQuery', true);
 
 
-mongoose.connect(DB_HOST)
-    .then(() => {
-        app.listen(PORT);
-    })
-    .catch(error => {
-        process.exit(1);
-    })
+// mongoose.connect(DB_HOST)
+//     .then(() => {
+//         app.listen(PORT);
+//     })
+//     .catch(error => {
+//         process.exit(1);
+//     })
